5aa59120221b233fee67bce21e72bdda68cdaa69
hossainchisty <hossain.chisty11@gmail.com>
2021-04-10 20:55:01 +0600

    add static root

--- Dear_Diary/settings.py
+++ Dear_Diary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,7 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ['static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
