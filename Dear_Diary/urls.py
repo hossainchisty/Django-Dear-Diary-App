4d94aecc6951109c6ea617c6bc7857c9e75c1e9a
hossainchisty <hossain.chisty11@gmail.com>
2021-04-10 20:15:17 +0600

    add dependency

--- Dear_Diary/urls.py
+++ Dear_Diary/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib import settings
+from django.views.static import serve
+from django.conf.urls import url 
+
 
 urlpatterns = [
     path('', include('Diary.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
