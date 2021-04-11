8d1f6f1634d5142efa00c9cdd4b426a773f0ea72
hossainchisty <hossain.chisty11@gmail.com>
2021-04-11 23:54:17 +0600

    Update views.py

--- Diary/views.py
+++ Diary/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
 from .models import Diary
@@ -12,6 +13,15 @@ def home(request):
         diary_post.save()
         
     diary_post = Diary.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(diary_post, 3)
+    try:
+        diary_post = paginator.page(page)
+    except PageNotAnInteger:
+        diary_post = paginator.page(1)
+    except EmptyPage:
+        diary_post = paginator.page(paginator.num_pages)
 
     context = {'diary_post':diary_post}
     return render(request, 'index.html', context)
