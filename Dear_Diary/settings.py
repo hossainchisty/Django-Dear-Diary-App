90d0e026182567eeaafbbe5cead787d8dbc945c3
hossainchisty <hossain.chisty11@gmail.com>
2021-05-01 17:30:59 +0600

    Update settings.py

--- Dear_Diary/settings.py
+++ Dear_Diary/settings.py
@@ -32,13 +32,15 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Thirt party 
+    'crispy_forms',
+    # local app
     'Diary.apps.DiaryConfig'
 ]
 
